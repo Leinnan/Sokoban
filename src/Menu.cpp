c4a655fbcea784caedd430654eb5bab52ce02b54
Leinnan <rybopiotr@gmail.com>
2016-11-28 20:05:54 +0100

    Start work on menu

---
+++ src/Menu.cpp
@@ -0,0 +1,14 @@
+//
+// Created by piotr on 22.11.16.
+//
+
+#include "Menu.h"
+
+namespace ar{
+
+    Menu::Menu(sf::RenderWindow *p_window_pointer){
+        this->window_pointer = p_window_pointer;
+
+
+    }
+}
